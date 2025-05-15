62c7892c89f6075f35c6d9d045eb3dca70604c17
itsmusili001 <musilivictoria9879@gmail.com>
2025-05-15 16:38:32 +0300

    day 27

--- WEEK6/day 27/exercise.py
+++ WEEK6/day 27/exercise.py
@@ -2,7 +2,7 @@ import sqlite3
 from tkinter import*
 
 root = Tk()
-root.title("books library")
+root.title("LIBRARY")
 root.geometry("500x500")
 conn=sqlite3.connect("books.db")
 c=conn.cursor()
@@ -29,11 +29,80 @@ def showBooks():
     
     display_books = ""
     for record in records:
-        display_books += f"{record[0]}- Price: {record[1]}\n"
+        display_books += f"{record[0]}-{record[1]} {record[2]}\n"
+
+    lblShowBooks=Label(root,text=display_books,justify="left")
+    lblShowBooks.grid(row=9,column=0,columnspan=2)
+
     
     lblShowBooks.config(text=display_books)
-    
     conn.close()
+
+def deleteBook():
+    conn = sqlite3.connect("books.db")
+    c = conn.cursor()
+    queryString="DELETE FROM BOOKS WHERE oid ="+txtBookId.get()
+    c.execute(queryString)
+    conn.commit()
+
+def updateBook():
+    updateWindow= Tk()
+    updateWindow.title("update book record")
+    updateWindow.geometry("500x500")
+    bookId=txtBookId.get()
+    conn = sqlite3.connect("books.db")
+    c = conn.cursor()
+    queryString="SELECT* FROM BOOKS WHERE oid =" +txtBookId.get()
+    c.execute(queryString)
+    records = c.fetchall()
+    
+    lblTitleUpdate =Label(updateWindow,text="update Books")
+    lblBookTitleUpdate=Label(updateWindow,text="update Book Title")
+    txtBookTitleUpdate=Entry(updateWindow, width=50)
+    lblTitleUpdate.grid(row=0,column=0,pady=10)
+    lblBookTitleUpdate.grid(row=0 ,column=0,pady=10)
+    txtBookTitleUpdate.grid(row=0 ,column=1,pady=10)
+
+    lblBookPriceUpdate =Label(updateWindow,text="update Book Price")
+    txtBookPriceUpdate =Entry(updateWindow, width=50)
+    lblBookPriceUpdate.grid(row=2 ,column=0,pady=10)
+    txtBookPriceUpdate.grid(row=2 ,column=1,pady=10)
+
+    for record in records:
+        txtBookTitleUpdate.insert(0,record[0])
+        txtBookPriceUpdate.insert(0,record[1])
+    def saveChanges():
+        conn = sqlite3.connect("books.db")
+        c = conn.cursor()
+        btitle=txtBookTitleUpdate.get()
+        bprice = txtBookPriceUpdate.get()
+        queryString = f"""UPDATE Books SET 
+        BookTitle = {btitle},
+        BookPrice = {bprice},
+        WHERE oid ={bookId},
+        """
+
+
+
+        # queryString="""UPDATE BOOKS SET
+        # BookTitle=:btitle,
+        # BookPrice=:bprice,
+        # WHERE rowid = oid
+        # """,{
+        #     'btitle':txtBookTitleUpdate.get(),
+        #     'bprice':txtBookPriceUpdate.get()
+        # 
+        c.execute(queryString)
+        conn.commit()
+        conn.close()
+        
+    btnUpdateBook=Button(updateWindow, text="save changes",command=saveChanges)
+    btnUpdateBook.grid(row=3,column=0 ,columnspan=2 ,pady=20)
+
+    conn.commit()
+    conn.close()
+
+
 
 lblTitle=Label(root,text="Books")
 lblBookTitle=Label(root,text="Book Title")
@@ -47,14 +116,23 @@ txtBookPrice=Entry(root, width=50)
 lblBookPrice.grid(row=2 ,column=0,pady=10)
 txtBookPrice.grid(row=2 ,column=1,pady=10)
 
-lblShowBooks=Label(root,text="display all books")
-lblShowBooks.grid(row=3,column=0,columnspan=2)
+lblBookId=Label(root,text = "Enter book ID")
+lblBookId.grid(row=6,column=0,pady=10)
+txtBookId=Entry(root,width=50)
+txtBookId.grid(row=6,column=1,pady=10)
 
 btnAddRecord=Button(root, text="add new book",command=addBook)
-btnAddRecord.grid(row=4,column=0 ,columnspan=2 ,ipadx=100)
+btnAddRecord.grid(row=4,column=0 ,columnspan=2 ,ipadx=90)
 
 btnShowBooks= Button(root, text="show books",command=showBooks)
 btnShowBooks.grid(row=5,column=0,columnspan=2 ,ipadx=100)
+
+btnDeleteBook= Button(root,text="delete book record",command=deleteBook)
+btnDeleteBook.grid(row=7,column=0,columnspan=2,ipadx=90)
+
+btnUpdateBook=Button(root,text="update books",command=updateBook)
+btnUpdateBook.grid(row=8,column=0,columnspan=2,ipadx=100)
+
 
 conn.commit()
 conn.close()
